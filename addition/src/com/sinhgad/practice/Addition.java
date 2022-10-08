bdf8162eb636f7b361645391d46b359c1cfda0f1
Chaitanya <115354543+chaitanya-218@users.noreply.github.com>
2022-10-08 18:48:08 +0530

    addition program

---
+++ addition/src/com/sinhgad/practice/Addition.java
@@ -0,0 +1,16 @@
+package com.sinhgad.practice;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// hard code first value
+		int firstValue = 10;
+		// hard code second value
+		int secondValue = 20;
+		
+		
+		System.out.println("Addition: " + (firstValue + secondValue));
+
+	}
+
+}
---
+++ addition/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author chaitanya
+ *
+ */
+module addition {
+}
